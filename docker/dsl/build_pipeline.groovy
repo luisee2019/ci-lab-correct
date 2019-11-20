f1af533ff52fa68d3047821afb2bf8f6a9d9bc97
CORP\espinal1 <none@espin.com>
2019-11-19 20:48:08 -0500

    Change to add Hello Luis

--- docker/dsl/build_pipeline.groovy
+++ docker/dsl/build_pipeline.groovy
@@ -1,7 +1,7 @@
 pipelineJob('build-pipeline-job') { 
 
   // This is the repo that contains the Jenkinsfile you want to use to define your pipeline
-  def repo = 'https://github.com/jschmersal-cscc/special-topics-labs-ci'
+  def repo = 'https://github.com/luisee2019/ci-lab-correct'
 
   description("Your App Pipeline") 
   keepDependencies(false) 
@@ -18,7 +18,7 @@ pipelineJob('build-pipeline-job') {
       scm { 
         git { 
           remote { url(repo) } 
-          branches('master') 
+          branches('changes-for-labCI')
           scriptPath('Jenkinsfile') // The location of the Jenkinsfile that defines the pipeline job within the repo
           extensions { }  // required as otherwise it may try to tag the repo, which you may not want 
         } 
